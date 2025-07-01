d0d483a60bd276936a8cc09a0b5da3a6316a9e51
faeld1 <faeldsnc@gmail.com>
2025-07-01 20:56:03 -0300

    AttackSpeed float mult

--- Assets/Scripts/Player/Player_Stats.cs
+++ Assets/Scripts/Player/Player_Stats.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class Player_Stats : CharacterStats
 {
+    private Player player;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        player = GetComponent<Player>();
+    }
+
     private int bonusAttack;
     private int bonusHealth;
     private int bonusSpeed;
@@ -17,6 +25,8 @@ public class Player_Stats : CharacterStats
         bonusAttack = atk;
         bonusHealth = hp;
         bonusSpeed = spd;
+
+        player.animator.SetFloat("AttackSpeed", 1f + (BonusSpeed * 0.1f) * 2);
 
         if (healthChanged)
             UpdateHealth(); // atualiza vida caso maxHealth tenha mudado
