9e6141f4f34e581510c3d529d4c5e10933f1ee37
faeld1 <44978899+faeld1@users.noreply.github.com>
2025-07-21 13:03:09 -0300

    Add hit FX event and handling

--- Assets/Scripts/Player/Player_FX.cs
+++ Assets/Scripts/Player/Player_FX.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class Player_FX : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform hitFxPosition;
+    private CharacterStats stats;
+
+    private void Awake()
     {
-        
+        stats = GetComponent<CharacterStats>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        if (stats != null)
+            stats.OnHit += PlayHitFx;
+    }
+
+    private void OnDisable()
+    {
+        if (stats != null)
+            stats.OnHit -= PlayHitFx;
+    }
+
+    private void PlayHitFx()
+    {
+        if (hitFxPosition == null || HitFxManager.instance == null)
+            return;
+
+        HitFxManager.instance.PlayHitFx(hitFxPosition.position, hitFxPosition.rotation);
     }
 }
